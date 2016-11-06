6efde25439aed96932b0d6125b5616eafa0c1f7c
noraworld <noraworld@mail.com>
2016-11-06 15:37:01 +0000

    :pencil2: Optimize loop contents

---
+++ assets/js/parallax.js
@@ -0,0 +1,26 @@
+if (!navigator.userAgent.match(/(iPhone|android)/)) {
+
+  $(window).scroll(function() {
+
+    if ($(window).width() > 500) {
+
+      var postImageNumber = $('.post-image').length;
+
+      for (var i = 0; i < postImageNumber; i++) {
+
+        // 画像のスクロールを開始/終了するタイミングを遅く/早くしたいときに scrollOffset の値を大きくする
+        var scrollOffset = 150;
+        var scrollPositionSet = $(window).height() - $('.post-image').eq(i).height() - 2 * scrollOffset;
+        var scrollPosition = 100 - ($('.post-image').eq(i).offset().top - $(this).scrollTop() - scrollOffset) / scrollPositionSet * 100;
+
+        if (scrollPosition > 0 && scrollPosition < 100) {
+          $('.post-image').eq(i).css('background-position-y', scrollPosition + '%');
+        }
+
+      }
+
+    }
+
+  });
+
+}
