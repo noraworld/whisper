feff1117d9036392b62744fce6a4318ae2305195
noraworld <noraworld@mail.com>
2016-11-09 11:10:24 +0000

    :recycle: Optimize blank lines on parallax.js

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -1,13 +1,9 @@
 if (!navigator.userAgent.match(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/)) {
-
   $(window).scroll(function() {
-
     if ($(window).width() > 500) {
-
       var postImageNumber = $('.post-image').length;
 
       for (var i = 0; i < postImageNumber; i++) {
-
         // 画像のスクロールを開始/終了するタイミングを遅く/早くしたいときに scrollOffset の値を大きくする
         var scrollOffset = 150;
         var scrollPositionSet = $(window).height() - $('.post-image').eq(i).height() - 2 * scrollOffset;
@@ -16,11 +12,7 @@ if (!navigator.userAgent.match(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobi
         if (scrollPosition > 0 && scrollPosition < 100) {
           $('.post-image').eq(i).css('background-position-y', scrollPosition + '%');
         }
-
       }
-
     }
-
   });
-
 }
