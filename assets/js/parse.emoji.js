afdacec441dc5d809951f84f772bc71fe50c3505
noraworld <noraworld@mail.com>
2017-01-31 04:45:31 +0900

    :bug: Exclude a copyright image loading (404) from iEmoji

--- assets/js/parse.emoji.js
+++ assets/js/parse.emoji.js
@@ -1,1 +1,11 @@
-twemoji.parse(document.body);
+twemoji.parse(document.body, {
+  callback: function(icon, options, vatiant) {
+    switch (icon) {
+      case   'a9':  // copyright
+      case   'ae':  // registered trademark
+      case '2122':  // trademark
+            return false;
+    }
+    return ''.concat(options.base, options.path, '/', icon, options.ext);
+  }
+});
